620644a8cc47add91e82fa97f5f3f7ddd4567d69
dasbiplob <biplob90.d@git.com>
2023-09-06 15:02:36 +0300

    WebApp Containrization with Docker

---
+++ app.js
@@ -0,0 +1,13 @@
+import { serve } from "https://deno.land/std@0.171.0/http/server.ts";
+import postgres from "https://deno.land/x/postgresjs@v3.3.3/mod.js";
+
+
+const sql = postgres({});
+const handleRequest = async (request) => {
+  const rows = await sql`SELECT COUNT(*) AS c FROM users`;
+  return new Response(`Meaning of life: ${rows[0].c}`);
+
+  console.log(Deno.env.get("PGHOST"));
+};
+
+serve(handleRequest, { port: 7777 });
